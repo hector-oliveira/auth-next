5d101e19c86745949ee129400386dd0132734f2a
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-06-08 13:37:11 -0300

    improve: input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,44 +1,32 @@
 "use client";
-import { iconDefaultProperties } from "constants/icon-default-properties.ts";
-import { useState } from "react";
-import { LuEyeOff, LuEye } from "react-icons/lu";
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  handleIcon?: () => JSX.Element;
-};
+import { renderPasswordIcons } from "functions/renderPasswordIcons";
+import { useInputSelected, useToggle } from "hooks";
+import { InputProps } from "interfaces/ElementsHTML";
+import { useVisibility } from "./hooks-input";
 
 export function Input({ handleIcon, ...rest }: InputProps) {
-  const [isVisible, setIsVisible] = useState(false);
+  const { handleToggle, isToggle } = useToggle();
+  const { inputSelectRef, handleInputSelection } = useInputSelected();
+  const handleVisibility = useVisibility({
+    handleToggle,
+    handleInputSelection,
+  });
+
   const displayedInputType =
-    rest.type === "password" && isVisible ? "text" : rest.type;
-
-  function handleVisibility() {
-    setIsVisible(!isVisible);
-  }
-
-  function renderPasswordIcons(isVisiblePassword: boolean) {
-    return (
-      <>
-        {isVisiblePassword ? (
-          <LuEyeOff {...iconDefaultProperties} />
-        ) : (
-          <LuEye {...iconDefaultProperties} />
-        )}
-      </>
-    );
-  }
+    rest.type === "password" && isToggle ? "text" : rest.type;
 
   return (
     <section className="border border-black py-2 px-2 gap-1 flex items-center bg-slate-200 rounded-md">
       {handleIcon && handleIcon()}
       <input
         {...rest}
+        ref={inputSelectRef}
         type={displayedInputType}
         className="w-full border-none outline-none bg-transparent hover:ease-in-out hover:duration-300"
       />
       {rest.type === "password" && (
         <button type="button" onClick={handleVisibility}>
-          {renderPasswordIcons(isVisible)}
+          {renderPasswordIcons(isToggle)}
         </button>
       )}
     </section>
